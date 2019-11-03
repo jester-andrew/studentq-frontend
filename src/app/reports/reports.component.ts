32cf9e30ffba1977d87d183d39190caa0b94b84b
alevijester <andrewjester92@gmail.com>
2019-11-02 19:02:50 -0600

    report functionality in place

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -24,6 +24,29 @@ export class ReportsComponent implements OnInit {
   //courses
   courses:any;
 
+  //filters
+  week:boolean;
+  month:boolean;
+  courseValue:string;
+
+  timeScope:string = 'week';
+  timeCourse:string;
+
+  //time values
+  wLong;
+  wShort;
+  wAvg;
+
+  hLong;
+  hShort;
+  hAvg;
+
+  //numbers helped
+  helpToday:number;
+  helpWeek:number;
+  helpMonth:number;
+  helpSemester:number;
+
   constructor(private report:ReportsService, private courseService: CourseService ) { }
 
   ngOnInit() {
@@ -46,13 +69,189 @@ export class ReportsComponent implements OnInit {
     });
   }
 
-  changeReportView(option){
+  changeReportScope(option){
+    console.log('called');
+    console.log(option);
     if(option == 1){
-
-    }else if(option == 2){
-
-    }else if(option == 3){
-
+      this.week = true;
+      this.month = false;
+      //weekly
+      if(this.courseValue == null || this.courseValue == undefined){
+        this.qrReports = this.bulkReport.week.sessions
+      }else{
+        this.qrReports = this.bulkReport.week.sessions.filter((value) => value.class == this.courseValue);
+      }
+    }else{
+      this.week = false;
+      this.month = true;
+      //monthly
+      if(this.courseValue == null || this.courseValue == undefined){
+        this.qrReports = this.bulkReport.month.sessions;
+      }else{
+        this.qrReports = this.bulkReport.month.sessions.filter((value) => value.class == this.courseValue);
+      }
     }
+  }
+
+  changeCourse(course){
+    if(course == 'All'){
+      this.courseValue = null;
+      if(this.week){
+        this.qrReports = this.bulkReport.week.sessions
+      }else{
+        this.qrReports = this.bulkReport.month.sessions
+      }
+    }else{
+      this.courseValue = course;
+
+      if(this.week){
+        this.qrReports = this.bulkReport.week.sessions.filter((value) => value.class == this.courseValue);
+      }else{
+        this.qrReports = this.bulkReport.month.sessions.filter((value) => value.class == this.courseValue);
+      }
+    }
+  }
+
+  changeReportView(reportOption){
+    console.log(reportOption);
+    if(reportOption == 1){ //question/report
+      this.questionReport = true;
+      this.times = false;
+      this.numbers = false;
+    }else if(reportOption == 2){//lab times
+      this.calculateTimes();
+      this.questionReport = false;
+      this.times = true;
+      this.numbers = false;
+    }else{//numbers helped
+      this.calculateNumbers();
+      this.questionReport = false;
+      this.times = false;
+      this.numbers = true;
+    }
+  }
+
+  private calculateTimes(){
+
+    let timeInMS = this.calculateMS();
+    let waitTimes = timeInMS.wait;
+    let helpTimes = timeInMS.help;
+
+    //calcumate max times 
+    this.wLong =  this.caluclateHourMinSec(Math.max(...waitTimes));
+    this.hLong =  this.caluclateHourMinSec(Math.max(...helpTimes));
+    
+    //calulate min times
+    this.wShort =  this.caluclateHourMinSec(Math.min(...waitTimes));
+    this.hShort =  this.caluclateHourMinSec(Math.min(...helpTimes));
+    
+    //calculate average times
+    this.wAvg = this.caluclateHourMinSec(this.calculateAVG(waitTimes));
+    this.hAvg = this.caluclateHourMinSec(this.calculateAVG(helpTimes));
+
+  }
+
+  caluclateHourMinSec(timeMS){
+    let hour = Math.floor(timeMS / 1000 / 60 / 60);
+    let hourMS = hour * 1000 * 60 * 60
+    console.log(hourMS);
+    let min = Math.floor((timeMS - hourMS) / 1000 / 60);
+    console.log((timeMS - hourMS)/1000/60);
+    let minMS = min * 1000 * 60;
+    let sec = Math.floor((timeMS - (hourMS + minMS)) / 1000 );
+
+
+    let time = {
+      hour: hour,
+      min: min, 
+      sec: sec
+    }
+
+    return time;
+  }
+
+  calculateAVG(numArray){
+    let total = 0;
+    let arrayLength = numArray.length;
+
+    for(let i = 0; i < arrayLength; i++){
+      total += numArray[i];
+    }
+
+   return Math.floor(total / arrayLength);
+  }
+
+  calculateMS(){
+    let waitTimes = [];
+    let helpTimes = [];
+    
+    let targetArray = this.bulkReport[this.timeScope].sessions;
+  
+    if(this.timeCourse != null || this.timeCourse != undefined){
+      targetArray = targetArray.filter((value) => value.class == this.timeCourse);
+    }
+
+    //get times in MS
+    for (let i = 0; i < targetArray.length; i++) {
+      let qtime = new Date(parseInt(targetArray[i].timeEnteredQue));
+      let htime = new Date(parseInt(targetArray[i].timeHelped));
+      let endTime = new Date(parseInt(targetArray[i].finishedHelp));
+      
+      let waitTimeMS:number = htime.getTime() - qtime.getTime();
+      let helptimeMS:number = endTime.getTime() - htime.getTime();
+  
+      waitTimes.push(waitTimeMS);
+      helpTimes.push(helptimeMS);
+
+      
+    }
+    //remove NaN values if a date isn't stored correctly
+    waitTimes = waitTimes.filter(value => value);
+    helpTimes = helpTimes.filter(value => value);
+    console.log(waitTimes);
+    console.log(helpTimes);
+    return {wait: waitTimes, help: helpTimes};
+  }
+
+  changeTimeScope(timeScope){
+    if(timeScope == 1){//weekly
+      this.timeScope = 'week';
+    }else if(timeScope == 2){//monthly
+      this.timeScope = 'month';
+    }else{//semester
+      this.timeScope = 'semester'
+    }
+
+    this.calculateTimes();
+  }
+
+  changeTimeCourse(course){
+    if(course == 'All'){
+      this.timeCourse = null;
+    }else{
+      console.log(course);
+      this.timeCourse = course;
+    }
+    this.calculateTimes();
+  }
+
+  calculateNumbers(){
+    this.helpWeek = this.bulkReport.week.sessions.length;
+    this.helpMonth = this.bulkReport.month.sessions.length;
+    this.helpSemester = this.bulkReport.semester.sessions.length;
+
+    let todayArray = [];
+    for(let i = 0; i < this.helpWeek; i++){
+      todayArray = this.bulkReport.week.sessions.filter((value) => this.isToday(new Date(value.finishedHelp)));
+    }
+
+    this.helpToday = todayArray.length;
+  }
+
+  isToday(someDate){
+    let today = new Date()
+    return someDate.getDate() == today.getDate() &&
+      someDate.getMonth() == today.getMonth() &&
+      someDate.getFullYear() == today.getFullYear()
   }
 }
